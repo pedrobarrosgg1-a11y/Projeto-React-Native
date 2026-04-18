de0d5d5c4d9e772c812ccebca531dbac44c90f27
Pedro <pedro.barros.gg1@gmail.com>
2026-04-17 22:06:33 -0300

    Sixth Commit✨

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,10 @@
-import { View, Image, TouchableOpacity } from "react-native"
+import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 
+import { Link } from "@/components/link"
 import { Categories } from "@/components/categories"
 
 export default function Index() {
@@ -21,6 +22,39 @@ export default function Index() {
       </View>
 
       <Categories />
+
+      
+
+      <FlatList
+      data={["1", "2", "3", "4", "5"]}
+      keyExtractor={item => item}
+      renderItem={() => (<Link 
+        name="Rocketseat" 
+        url="https://www.rocketseat.com.br/" 
+        onDetails={() => console.log("Clicou!")} 
+      />
+    )}
+    style={styles.links}
+    contentContainerStyle={styles.linksContent}
+    showsVerticalScrollIndicator={false}
+      />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+            <TouchableOpacity>
+              <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+            </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkName}>Rocketseat </Text>
+
+            <Text style={styles.modalUrl}>https://www.rocketseat.com.br/</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -25,4 +25,44 @@ export const styles = StyleSheet.create({
         height: 32,
         width: 38,
     },
-});
+    links: {
+        borderTopWidth: 1,
+        borderTopColor: colors.gray[600]
+    },
+    linksContent: {
+        gap: 20,
+        padding: 24,
+        paddingBottom: 100,
+    },
+    modal: {
+        flex: 1,
+        justifyContent: "flex-end"
+    },
+    modalContent: {
+        backgroundColor: colors.gray[900],
+        borderTopWidth: 1,
+        borderTopColor: colors.gray[400],
+        paddingBottom: 42,
+        padding: 24,
+    },
+    modalHeader: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 32
+    },
+    modalCategory: {
+        flex: 1,
+        fontSize: 16,
+        fontWeight: "500",
+        color: colors.gray[400]
+    },
+    modalLinkName: {
+        fontSize: 18,
+        fontWeight: "600",
+        color: colors.gray[200]
+    },
+    modalUrl: {
+        color: colors.gray[400],
+    },
+})
