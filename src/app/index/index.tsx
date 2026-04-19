9af4fafff7d0771c3c6e7f0b45088e90f8cf8be4
Pedro <pedro.barros.gg1@gmail.com>
2026-04-19 18:59:27 -0300

    Seventh Commit✨

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,6 +5,7 @@ import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 
 import { Link } from "@/components/link"
+import { Option } from "@/components/option"
 import { Categories } from "@/components/categories"
 
 export default function Index() {
@@ -52,6 +53,11 @@ export default function Index() {
             <Text style={styles.modalLinkName}>Rocketseat </Text>
 
             <Text style={styles.modalUrl}>https://www.rocketseat.com.br/</Text>
+
+            <View style={styles.modalFooter}>
+            <Option name="Excluir" icon="delete" variant="secondary" />
+            <Option name="Abrir" icon="language" />
+            </View>
           </View>
         </View>
       </Modal>
