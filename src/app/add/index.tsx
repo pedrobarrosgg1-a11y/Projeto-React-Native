332a9f2efd9170ff5775dccd495bd98048d73f8d
Pedro <pedro.barros.gg1@gmail.com>
2026-04-27 20:27:25 -0300

    Error selecting category❌

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,11 +1,33 @@
-import { View, Text, TouchableOpacity } from "react-native"
+import { useState } from "react"
+import { View, Text, TouchableOpacity, Alert } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
-import { router } from "expo-router"
+
+import { categories } from "@/utils/categories"
+import { Categories } from "@/components/categories"
+import { Input } from "@/components/input"
+import { Button } from "@/components/button"
 
 export default function Add() {
+    const [category, setCategory] = useState("")
+    const [ name, setName] = useState<string>()
+    const [url, setUrl] =useState<string>()
+
+    function handleAdd(){
+        if(!category){
+            return Alert.alert("Categoria", "Selecione a categoria")
+        }
+
+        if(!name?.trim( )){
+            return Alert.alert("Nome", "Informe o Nome")
+        }
+
+        console.log({ category, name, url})
+    }
+
     return (
         <View style={styles.container}>
 
@@ -41,7 +63,13 @@ export default function Add() {
               Assim fica abaixo da linha com seta + "Novo"
             */}
             <Text style={styles.label}>Selecione uma Categoria</Text>
+            <Categories onChange={setCategory} selected={category}/>
 
+            <View style={styles.form}>
+                <Input placeholder="Nome" onChangeText={setName} autoCorrect={false}/>
+                <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false}/>
+                <Button title="Adicionar" onPress={handleAdd} />
+            </View>
         </View>
     )
 }
